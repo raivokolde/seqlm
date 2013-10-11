6f78e2b01bc82cebed67a0d4bb71c58f2be0e36e
Raivo Kolde <rkolde@gmail.com>
2013-10-11 12:02:44 +0300

    Added the the external package import

--- R/seqlm-package.r
+++ R/seqlm-package.r
@@ -3,4 +3,13 @@
 #' @name seqlm
 #' 
 #' @docType package
+#' @import foreach
+#' @import reshape
+#' @import multcomp
+#' @import mvtnorm
+#' @import plyr
+#' @import ggplot2
+#' @import Matrix
+#' 
+#' @aliases seqlm-package
 NULL
